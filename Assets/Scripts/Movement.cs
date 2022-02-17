0f51bbb8e43dff859af8643c0a87e1b12de85221
edu1010 <39130123+edu1010@users.noreply.github.com>
2022-02-17 02:16:06 +0100

    Movement and rotation of the board

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,28 +12,38 @@ public class Movement : MonoBehaviour, IPlayerActions
     private Vector3 movement;
     bool m_IsMoving = false;
     public GameObject m_Camera;
+    public GameObject m_Renderer;
+
+    public float m_RotationQuantity = 15;
+    public float m_TimeToRotate = 1.5f;
+    Quaternion m_TargetRotation = Quaternion.identity;
+    bool m_root = false;
+    float rateTiempo;
 
     // Start is called before the first frame update
     void Start()
     {
         m_rigidbody = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        //m_rigidbody.AddForce(transform.forward * m_force, ForceMode.Force);
         if (m_IsMoving)
         {
-            
             m_rigidbody.AddForce(movement * m_force, ForceMode.Force);
         }
-
-        //Vector2 inputDir = _input.Player.Movement.ReadValue<Vector2>();
+        if (m_root)
+        {
+            RotateRenderer();
+        }
+        else
+        {
+            m_TargetRotation = Quaternion.identity;
+            RotateRenderer();
+        }
     }
-
-
     public void OnMove(InputAction.CallbackContext context)
     {
         Vector3 l_Forward = m_Camera.transform.forward;
@@ -45,25 +55,34 @@ public class Movement : MonoBehaviour, IPlayerActions
         l_Right.Normalize();
         switch (context)
         {
-            case var value when context.started:
+            case var value when !context.canceled:
             movement = Vector3.zero;
             if (context.ReadValue<Vector2>().x > 0)
             {
-                movement += l_Right;
+                    movement += l_Right;
+                    CalculateRotateRenderer(true);
             }
             else if (context.ReadValue<Vector2>().x < 0)
             {
-                movement -= l_Right;
+                    movement -= l_Right;
+                    CalculateRotateRenderer(false);
             }
+            else
+            {
+            m_root = false;
+            }
+
             if (context.ReadValue<Vector2>().y > 0)
                 movement += l_Forward;
             else if (context.ReadValue<Vector2>().y < 0)
                 movement -= l_Forward;
-            
+
+                movement.Normalize();
                 m_IsMoving = true;
                     break;
             case var value when context.canceled:
                 m_IsMoving = false;
+                m_root = false;
                 break;
             
         }
@@ -78,4 +97,34 @@ public class Movement : MonoBehaviour, IPlayerActions
     {
         throw new System.NotImplementedException();
     }
+
+    public void CalculateRotateRenderer(bool right)
+    {
+        if (right)
+        {
+            m_TargetRotation = Quaternion.Euler(m_Renderer.transform.rotation.eulerAngles.x,
+                m_Renderer.transform.rotation.eulerAngles.y,
+                - m_RotationQuantity);
+        }
+        else
+        {
+            m_TargetRotation = Quaternion.Euler(m_Renderer.transform.rotation.eulerAngles.x,
+               m_Renderer.transform.rotation.eulerAngles.y,
+               m_RotationQuantity);
+        }
+        m_root = true;
+    }
+
+    void RotateRenderer()
+    {
+       
+        rateTiempo = 1f / m_TimeToRotate;
+        float t = 0.0f;
+        if (t <= 1f)
+        {
+            t += Time.deltaTime * rateTiempo;
+            m_Renderer.transform.rotation = Quaternion.Lerp(m_Renderer.transform.rotation, m_TargetRotation, t);
+        }
+    }
+
 }
